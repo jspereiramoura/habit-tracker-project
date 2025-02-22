4ec725079e96dfe10b9b3336508b526b63cb7c13
jspereira <joseluiz.zp@gmail.com>
2025-02-22 12:18:58 -0300

    feat: add test coverage configuration for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: ["src/test/config.ts"]
+    setupFiles: ["src/test/config.ts"],
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html", "lcov"],
+      include: ["src/app/**/*"],
+      reportsDirectory: "src/test/coverage",
+      extension: [".js", ".jsx", ".ts", ".tsx"]
+    }
   }
-})
+});
