c2827fc7bc52c9ed2f26a785d051c730b41fa4ba
jspereira <joseluiz.zp@gmail.com>
2025-02-21 21:21:31 -0300

    fix: conditionally render navigation links in Header based on current path

--- src/app/_components/header/index.tsx
+++ src/app/_components/header/index.tsx
@@ -1,8 +1,13 @@
-import { ComponentProps } from "react";
+"use client"
+
+import { ComponentProps, useEffect, useState } from "react";
 import Button from "../button";
 import LogoIcon from "../icons/Logo";
+import Link from "next/link";
 
 const Header = () => {
+  const [mustShowRegisterLinks, setMustShowRegisterLinks] = useState(false);
+
   const navLinks: ComponentProps<typeof Button>[] = [
     { as: "a", content: "Entrar", href: "/login" },
     {
@@ -13,22 +18,28 @@ const Header = () => {
     }
   ];
 
+  useEffect(() => {
+    setMustShowRegisterLinks(window.location.pathname === "/");
+  }, []);
+
   return (
     <header className="sticky top-0 border-b-1 border-primary">
       <div className="flex flex-wrap gap-4 py-4 px-5 container items-center justify-between">
-        <div className="flex items-center space-x-4">
+        <Link className="flex items-center space-x-4" href="/">
           <LogoIcon />
           <h1 className="font-montserrat font-semibold ">Owl Habit Tracker</h1>
-        </div>
-        <nav className="max-[550]:w-full">
-          <ul className="flex gap-4">
-            {navLinks.map((link, index) => (
+        </Link>
+        {mustShowRegisterLinks && (
+          <nav className="max-[550]:w-full">
+            <ul className="flex gap-4">
+              {navLinks.map((link, index) => (
                 <li key={index} className="flex-1">
-                <Button {...link} className="w-full" />
+                  <Button {...link} className="w-full" />
                 </li>
-            ))}
-          </ul>
-        </nav>
+              ))}
+            </ul>
+          </nav>
+        )}
       </div>{" "}
     </header>
   );
