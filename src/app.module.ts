ab8a1aa20671960c0e3836ee1a6f1de6ee6ad8d2
jspereira <joseluiz.zp@gmail.com>
2025-02-27 17:30:07 -0300

    refactor: moving db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { DbModule } from "./db/db.module";
+import { DbModule } from "./config/db/db.module";
 
 @Module({
   imports: [DbModule],
