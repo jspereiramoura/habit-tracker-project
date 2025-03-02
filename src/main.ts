c886f979ff5b72861e63dbaa690dcbd802756b0d
jspereira <joseluiz.zp@gmail.com>
2025-03-01 22:18:54 -0300

    feat: add CORS configuration and update .env.sample for local development

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from "./app.module";
 import { setupSwagger } from "./config/docs/swagger.config";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: process.env.CORS_ORIGIN,
+      allowedHeaders: "Content-Type, Authorization, Accept",
+      methods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+      credentials: true
+    }
+  });
 
   setupSwagger(app);
   await app.listen(process.env.PORT ?? 3000);
