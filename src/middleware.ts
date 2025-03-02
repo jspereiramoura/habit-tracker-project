3c88d6819ca0b6c932e259c4f5b6f4557dc50914
jspereira <joseluiz.zp@gmail.com>
2025-03-01 23:16:50 -0300

    feat: implement authentication middleware with redirection logic and associated tests

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("token");
+
+  const { pathname } = request.nextUrl;
+
+  if (pathname.startsWith("/dashboard") && !token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+
+  if (
+    token &&
+    (pathname.startsWith("/entrar") || pathname.startsWith("/registrar"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+}
