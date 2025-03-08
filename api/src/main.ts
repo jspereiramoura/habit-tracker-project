10a1b123c2459e7b81eded2d7242c13b7e313329
jspereira <joseluiz.zp@gmail.com>
2025-03-08 09:50:22 -0300

    chore: adjusting for deployment

--- api/src/main.ts
+++ api/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   });
 
   setupSwagger(app);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, process.env.HOST ?? "0.0.0.0");
 }
 bootstrap();
