ea5ed58d9310bbdf9bf8ffe369a338c488a98685
José Pereira <contato@jspereiramoura.dev.br>
2025-09-05 17:17:36 -0300

    feat: refactor Datadog initialization into a separate function

--- web/src/app/_components/datadogInit.tsx
+++ web/src/app/_components/datadogInit.tsx
@@ -1,32 +1,43 @@
 "use client";
 
 import { datadogRum } from "@datadog/browser-rum";
+import { useEffect } from "react";
 
 
-datadogRum.init({
+function initDatadog() {
+    datadogRum.init({
+        
+        applicationId: '',
+        clientToken: '',
+        // `site` refers to the Datadog site parameter of your organization
+        // see https://docs.datadoghq.com/getting_started/site/
+        site: 'us5.datadoghq.com',
+        service: 'habit-tracker-frontend',
+        env: 'production',
+        // Specify a version number to identify the deployed version of your application in Datadog
+        // version: '1.0.0',
+        sessionSampleRate: 100,
+        sessionReplaySampleRate: 20,
+        trackBfcacheViews: true,
+        defaultPrivacyLevel: 'mask-user-input',
+        trackUserInteractions: true,
+        trackResources: true,
+        trackLongTasks: true,
+        trackViewsManually: false,
+        allowedTracingUrls: [
+            { match: "https://api.habits.jspereiramoura.dev.br/", propagatorTypes: ["tracecontext"] },
+        ],
+    });
 
-    applicationId: '',
-    clientToken: '',
-    // `site` refers to the Datadog site parameter of your organization
-    // see https://docs.datadoghq.com/getting_started/site/
-    site: 'us5.datadoghq.com',
-    service: 'habit-tracker-frontend',
-    env: 'production',
-    // Specify a version number to identify the deployed version of your application in Datadog
-    // version: '1.0.0',
-    sessionSampleRate: 100,
-    sessionReplaySampleRate: 20,
-    trackBfcacheViews: true,
-    defaultPrivacyLevel: 'mask-user-input',
-    trackUserInteractions: true,
-    trackResources: true,
-    trackLongTasks: true,
-    allowedTracingUrls: [
-        { match: "https://api.habits.jspereiramoura.dev.br/", propagatorTypes: ["tracecontext"] },
-    ],
-});
+    datadogRum.startSessionReplayRecording();
+}
 
 export default function DatadogInit() {
+    useEffect(() => {
+        initDatadog();
+        console.log("Datadog RUM initialized");
+        
+    }, [])
     return null;
 }
 
